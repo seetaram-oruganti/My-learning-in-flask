bfd2fcb353147a51a688a16d3e0211c595554428
Seetaram_oruganti <orugantiseetaram@gmail.com>
2020-05-07 18:41:43 +0530

    Update tutorial_1.py

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for
 
 
-app = Flask(__name__)
+app = Flask(__name__)       #instansiation of the page 
 
 
-@app.route("/")
-def home():
-    return "Hello! this is Flask<h1>HELLO Flask!<h1>"
-@app.route("/<name>")
+@app.route("/")                                         """"********"""
+def home():                                                    """ *****"""
+    return "Hello! this is Flask<h1>HELLO Flask!<h1>"         """  main page """
+@app.route("/<name>")                                           
 def user(name):
     return f"Hello, {name}!"  
 
